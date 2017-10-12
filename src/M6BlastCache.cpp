d473afa1abd9208dc92ddf04c4bc18c5c9899a41
Coos Baakman <cbaakman@radboudumc.nl>
2017-10-12 13:42:51 +0200

    whoops! syntax

--- src/M6BlastCache.cpp
+++ src/M6BlastCache.cpp
@@ -305,7 +305,7 @@ void M6BlastCache::CacheResult(const string& inJobID, M6BlastResultPtr inResult)
         out.push(file);
 
         out << doc;
-        out.close();
+        file.close();
 
         // Place the job with requested id at the beginning of the list
         auto j = i;
@@ -451,7 +451,7 @@ void M6BlastCache::StoreJob(const string& inJobID, const M6BlastJob& inJob)
 
     fs::ofstream file(mCacheDir / (inJobID + ".job"), ios_base::out|ios_base::trunc);
     file << doc;
-    file.close()
+    file.close();
 }
 
 bool IsHighLoad (const M6BlastJob &job)
